9baf386f9042eee942ad7c3d3416332aa9ce2c75
deammer <deammercraft@gmail.com>
2015-06-11 22:58:04 -0500

    #Draft 4b
    Messed with the homing projectiles.

--- Star Slayer/Assets/Scripts/HomingProjectile.cs
+++ Star Slayer/Assets/Scripts/HomingProjectile.cs
@@ -20,10 +20,19 @@ public class HomingProjectile : Projectile
 		var dx = target.position.x - transform.position.x;
 		var dy = target.position.y - transform.position.y;
 
-		if (velocity.y * dx - velocity.x * dy > 0)
+		/*var dotProduct = velocity.y * dx - velocity.x * dy;
+		if (dotProduct > 0)
 			angle += turnRate * Time.deltaTime;
 		else
-			angle -= turnRate * Time.deltaTime;
+			angle -= turnRate * Time.deltaTime;*/
+
+		Vector2 direction = transform.position - target.position;
+		//direction = target.InverseTransformDirection(direction);
+		var targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; // between -180 and 180
+		if (Mathf.Abs(targetAngle - angle) <= turnRate * Time.deltaTime)
+			angle = targetAngle;
+	//	else
+	//		angle = Mathf.Sign (a) * turnRate * Time.deltaTime;
 
 		while (angle < 0) angle += 360f;
 		while (angle > 360) angle -= 360f;
