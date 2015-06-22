7b960e5ecbb2e31e14a5b6adba6a0d01e7bcb79b
deammer <deammercraft@gmail.com>
2015-06-21 23:14:20 -0500

    PreProd #11
    Messed with iTween some more.

---
+++ Star Slayer/Assets/Scripts/PathMover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTweenPath path = GetComponent<iTweenPath>();
+		if (path != null)
+			iTween.MoveTo(gameObject, iTween.Hash("path", path.nodes.ToArray(), "time", 5, "easetype", iTween.EaseType.linear));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
